b0669b2d2e8b317a2814dc389ee8090276bc04d2
VladNegoita <vlad1negoita@gmail.com>
2023-04-29 18:06:27 +0300

    Added QR shift2

--- EigenValues/QR_simple.m
+++ EigenValues/QR_simple.m
@@ -1,7 +1,17 @@
-function A = QR_simple (A, max_iter = 10000)
-	
+function A = QR_simple (A, max_iter = 10000, tol = 1e-5)
+
 	for i = 1:max_iter
 		[Q, R] = qr(A);
-		A = R * Q;
+		
+		new_A = R * Q;
+		
+		if (norm(A - new_A) < tol)
+			break;
+		endif
+		
+		disp(i);
+
+		A = new_A;
 	endfor
+
 endfunction
