a830683a6aa49982cda84590e184e9abcb5075c8
Vlad Negoita <vlad1negoita@gmail.com>
2024-04-01 22:52:08 +0300

    Added SOR analysis function

---
+++ Iterative methods/sor_analysis.m
@@ -0,0 +1,8 @@
+function sor_analysis(A, b, x0, step = 0.05, w_start = 0.1, w_end = 1.95)
+  iters = [];
+  for w = w_start : step : w_end
+    [~, iters(end + 1)] = sor(A, b, x0, w, 1e-12);
+  endfor
+
+  plot(w_start : step : w_end, iters);
+endfunction
