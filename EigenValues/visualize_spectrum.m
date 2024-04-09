3ca0118e43657785c79393ce283519a67ef209e0
Vlad Negoita <vlad1negoita@gmail.com>
2024-04-09 11:59:48 +0300

    Added power method visualisation

---
+++ EigenValues/visualize_spectrum.m
@@ -0,0 +1,17 @@
+function visualize_spectrum(A, cnt = 100)
+  [n, ~] = size(A);
+
+  if n == 2
+    thetas = 0 : (2 * pi / cnt) : 2 * pi;
+    transformed = [cos(thetas); sin(thetas)];
+    transformed = A * transformed;
+
+    xs = transformed(1, :);
+    ys = transformed(2, :);
+    compass(xs, ys);
+  endif
+
+  if n == 3
+    % TODO
+  endif
+endfunction
